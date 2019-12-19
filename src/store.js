c0d48668d5cce8c861b6d18fb3f9158edaf68f5e
bigbanonos <55335906+bigbanonos@users.noreply.github.com>
2019-12-19 16:40:54 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -9,7 +9,7 @@ const state = {
     {
       offset: 1,
       factor: 1.75,
-      header: "Jefferson",
+      header: "December 19",
       image: "/photo-1515036551567-bf1198cccc35.jpeg",
       aspect: 1.51,
       text: "Coquito Coquito Coquito Coquito Coquito Coquito."
@@ -17,20 +17,20 @@ const state = {
     {
       offset: 2,
       factor: 2.0,
-      header: "Antonella",
+      header: "Alexis",
       image: "/photo-1519608487953-e999c86e7455.jpeg",
       aspect: 1.5,
       text:
-        "Jefferson recently enjoy post Thanksgiving empanadas with his favorite J. Just ask him............This Christmas, Lady Antonella is celebrating the true meaning of Christmas by giving you zero gifts, but she did bring strawberry rhubarb pie near the old devices on floor 3. She didn't like the vegan almond butter. She did appreciate the water refill."
+        "Alexis. Big Bird. Ernie. Cookie Monster. Bet"
     },
     {
       offset: 3,
       factor: 2.25,
-      header: "Dominique",
+      header: "Jefferson",
       image: "/ph1.jpg",
       aspect: 1.5037,
       text:
-        "Dominique Young Unique recently spent a week with her sister and niece or nephew. Went to Ireland. Iceland. I'm assuming Ecuador. If you're reading this on December 19, 2019, she has cool blue pants on."
+        "Jefferson recently enjoy post Thanksgiving empanadas with his favorite J. Just ask him."
     },
     {
       offset: 4,
@@ -44,13 +44,13 @@ const state = {
     {
       offset: 5,
       factor: 1.75,
-      header: "Sector 8",
+      header: "Antonella",
       image: "/photo-1533577116850-9cc66cad8a9b.jpeg",
       aspect: 1.55,
       text:
-        "By cultivating the state of mind that makes it possible for the dazzling ecstatic insights to become permanent and habitual illuminations. By getting to know oneself to the point where one won’t be compelled by one’s unconscious to do all the ugly, absurd, self-stultifying things that one so often finds oneself doing."
+        "This Christmas, Lady Antonella is celebrating the true meaning of Christmas by giving you zero gifts, but she did bring strawberry rhubarb pie near the old devices on floor 3. She didn't like the vegan almond butter. She did appreciate the water refill."
     },
-    { offset: 7, factor: 1.05, header: "The Factory", image: "/photo-1548191265-cc70d3d45ba1.jpeg", aspect: 1.77, text: "Education and enlightenment." }
+    { offset: 7, factor: 1.05, header: "Dominque", image: "/photo-1548191265-cc70d3d45ba1.jpeg", aspect: 1.77, text: "Dominique Young Unique recently spent a week with her sister and niece or nephew. Went to Ireland. Iceland. I'm assuming Ecuador. If you're reading this on December 19, 2019, she has cool blue pants on." }
   ],
   stripes: [
     { offset: 0, color: "#000", height: 13 },
